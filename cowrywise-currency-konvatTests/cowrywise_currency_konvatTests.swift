5788d5275e8ab03a523b6fe2817ed3089e82e28c
devayz <fatokimiemman@gmail.com>
2025-03-26 17:53:12 +0100

    Initial Commit

---
+++ cowrywise-currency-konvatTests/cowrywise_currency_konvatTests.swift
@@ -0,0 +1,17 @@
+//
+//  cowrywise_currency_konvatTests.swift
+//  cowrywise-currency-konvatTests
+//
+//  Created by Ayokunle Fatokimi on 26/03/2025.
+//
+
+import Testing
+@testable import cowrywise_currency_konvat
+
+struct cowrywise_currency_konvatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
