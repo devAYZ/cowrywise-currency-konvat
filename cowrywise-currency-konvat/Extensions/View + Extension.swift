c3f7a76fadc1988acc10cf556821426f60089e4f
devayz <fatokimiemman@gmail.com>
2025-03-27 10:52:20 +0100

    attach all ui components

---
+++ cowrywise-currency-konvat/Extensions/View + Extension.swift
@@ -0,0 +1,38 @@
+//
+//  View + Extension.swift
+//  cowrywise-currency-konvat
+//
+//  Created by Ayokunle Fatokimi on 27/03/2025.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
--- cowrywise-currency-konvat/ViewController.swift
+++ cowrywise-currency-konvat/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var exchangeRateButton: UIButton!
+    
+    private var exchangeRateButtonTitle = "Mid-market exchange rate at {t}  "
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        exchangeRateButton.setTitle(exchangeRateButtonTitle, for: .normal)
     }
 
 
